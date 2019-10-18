b7b2d1b7d7c07f47409b7cc46b8768754b4db619
singha42 <43796015+singha42@users.noreply.github.com>
2019-10-18 11:46:55 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,27 +10,32 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'GridAPPS-D DNP3 Service'
-copyright = '2019, Alka, Craig, Carrie, Andy'
+copyright = '2019, Pacific Northwest National Laboratory'
 author = 'Alka, Craig, Carrie, Andy'
 
 # The full version, including alpha/beta/rc tags
-release = '1'
+release = '2019.10.0'
 
 
 # -- General configuration ---------------------------------------------------
+
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+     'sphinx.ext.viewcode',
+     'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
