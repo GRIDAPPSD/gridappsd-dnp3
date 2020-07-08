5279a222e0a8de4a461c5d2b8dbf96dad054f96a
singha42 <alka.singh@pnnl.gov>
2020-07-08 13:32:08 -0400

    cim_to_dnp3.py

--- dnp3/service/dnp3/cim_to_dnp3.py
+++ dnp3/service/dnp3/cim_to_dnp3.py
@@ -6,7 +6,7 @@ import random
 import uuid
 import math
 
-from pydnp3 import opendnp3
+from pydnp3 import opendnp3, openpal, asiopal, asiodnp3
 from typing import List, Dict, Union, Any
 from dnp3.outstation import DNP3Outstation
 from dnp3.points import (
@@ -101,7 +101,8 @@ class DNP3Mapping():
                            angle2 = math.radians(m.get("angle"))
                            #point.magnitude = math.cos(angle1) * m.get("magnitude")
                            self.outstation.apply_update(opendnp3.Analog(angle2), point.index)
-
+                           
+                       
                 elif "value" in m.keys():
                     for point in self.outstation.get_agent().point_definitions.all_points():
                         if m.get("measurement_mrid") == point.measurement_id and point.value != m.get("value"):
@@ -109,6 +110,71 @@ class DNP3Mapping():
                              self.outstation.apply_update(opendnp3.Binary(point.value), point.index)
         except Exception as e:
             message_str = "An error occurred while trying to translate the  message received" + str(e)
+
+    def create_message_updates(self, simulation_id, message):
+        """ This method creates an atomic "updates" object for any outstation to consume via their .Apply method.
+        ----------
+        headers: dict
+            A dictionary of headers that could be used to determine topic of origin and
+            other attributes.
+        message: object
+
+        """
+        try:
+            message_str = 'received message ' + str(message)
+
+            builder = asiodnp3.UpdateBuilder()
+            json_msg = yaml.safe_load(str(message))
+
+            if type(json_msg) != dict:
+                raise ValueError(
+                    ' is not a json formatted string.'
+                    + '\njson_msg = {0}'.format(json_msg))
+
+            # fncs_input_message = {"{}".format(simulation_id): {}}
+            measurement_values = json_msg["message"]["measurements"]
+
+            # storing the magnitude and measurement_mRID values to publish in the dnp3 points for measurement key values
+            for y in measurement_values:
+                # print(self.processor_point_def.points_by_mrid())
+                m = measurement_values[y]
+                if "magnitude" in m.keys():
+                   for point in self.outstation.get_agent().point_definitions.all_points():
+                       #print("point",point)
+                       #print("y",y)
+                       if m.get("measurement_mrid") == point.measurement_id and point.magnitude != float(m.get("magnitude")):
+                           point.magnitude = float(m.get("magnitude"))
+                           builder.Update(opendnp3.Analog(point.magnitude), point.index)
+
+                       elif "VAR" in point.name:
+                           angle = math.radians(m.get("angle"))
+                           point.magnitude = math.sin(angle) * float(m.get("magnitude"))
+                           builder.Update(opendnp3.Analog(point.magnitude), point.index)
+                       
+                       elif "Watts"  in point.name:
+                           angle1 = math.radians(m.get("angle"))
+                           point.magnitude = math.cos(angle1) * float(m.get("magnitude"))
+                           builder.Update(opendnp3.Analog(point.magnitude), point.index)
+                       
+                       elif point.measurement_type == "VA" and "angle"  in point.name:
+                           angle2 = math.radians(m.get("angle"))
+                           #point.magnitude = math.cos(angle1) * m.get("magnitude")
+                           builder.Update(opendnp3.Analog(angle2), point.index)
+                           
+                       # elif "Net" in point.name:
+                           # if 
+
+                elif "value" in m.keys():
+                    for point in self.outstation.get_agent().point_definitions.all_points():
+                        if m.get("measurement_mrid") == point.measurement_id and point.value != m.get("value"):
+                             point.value = m.get("value")
+                             builder.Update(opendnp3.Binary(point.value), point.index)
+
+            print("Updates Created")
+            return builder.Build()
+        except Exception as e:
+            message_str = "An error occurred while trying to translate the  message received" + str(e)
+
 
     def assign_val_a(self, data_type, group, variation, index, name, description, measurement_type, measurement_id):
         """ Method is to initialize  parameters to be used for generating  output  points for measurement key values """
@@ -219,7 +285,7 @@ class DNP3Mapping():
 
 
             elif m['MeasurementClass'] == "Discrete" and  measurement_type == "Pos":
-                if "Reg" in m['name']:
+                if "RatioTapChanger" in m['name'] or "reg" in m["SimObject"]:
                     for r in range(5, 7):
                         self.assign_val_d("AO", 42, 3, self.c_ao, name, description,  measurement_id, attribute[r])
                         self.c_ao += 1
@@ -245,86 +311,108 @@ class DNP3Mapping():
                 self.assign_val_d("DO", 12, 1, self.c_do, name, description, measurement_id, cap_attribute[4])
                 self.c_do += 1
 
-        # for m in regulators:
-        #     reg_attribute = attribute_map['regulators']['attribute']
-        #     # bank_phase = list(m['bankPhases'])
-        #     for n in range(0, 4):
-        #         measurement_id = m.get("mRID")
-        #         name = uuid.uuid4().hex
-        #         description = "Name:" + m['bankName'] + ",ConductingEquipment_type:RatioTapChanger_Reg" +",Phase:" + m['bankPhases'] + ",Attribute:" + reg_attribute[n]
-        #         self.assign_val_d("AO", 42, 3, self.c_ao, name, description, measurement_id[0], reg_attribute[n])
-        #         self.c_ao += 1
-        #     for i in range(5, 7):
-        #         for j in range(0, len(m['bankPhases'])):
-        #             measurement_id = m.get("mRID")[j]
-        #             name = uuid.uuid4().hex
-        #             description = "Name:" + m['tankName'][j] + ",ConductingEquipment_type:RatioTapChanger_Reg"+ ",Phase:" + m['bankPhases'][j] + ",controlAttribute:" + reg_attribute[i]
-        #             self.assign_val_d("AO", 42, 3, self.c_ao, name, description, measurement_id,reg_attribute[i])
-        #             self.c_ao += 1
-        #
-        # for m in solarpanels:
-        #     measurement_id = m.get("mRID")
-        #     name = uuid.uuid4().hex
-        #     description = "Solarpanel:" + m['name'] + ",Phase:" + m['phases'] + ",measurementID:" + measurement_id
-        #     self.assign_val_a("AO", 42, 3, self.c_ao, name, description, None, measurement_id)
-        #     self.c_ao += 1
-
-        # for m in batteries:
-        #     measurement_id = m.get("mRID")
-        #     name = uuid.uuid4().hex
-        #     description = "Battery, " + m['name'] + ",Phase: " + m['phases'] + ",ConductingEquipment_type:PowerElectronicConnections"
-        #     self.assign_val_a("AO", 42, 3, self.c_ao, name, description, None, measurement_id)
-        #     self.c_ao += 1
+        for m in regulators:
+            reg_attribute = attribute_map['regulators']['attribute']
+            # bank_phase = list(m['bankPhases'])
+            for n in range(0, 4):
+                measurement_id = m.get("mRID")
+                name = m['bankname'] + '-' + m['bankPhases']
+                description = "Name:" + m['bankName'] + ",ConductingEquipment_type:RatioTapChanger_Reg" +",Phase:" + m['bankPhases'] + ",Attribute:" + reg_attribute[n]
+                self.assign_val_d("AO", 42, 3, self.c_ao, name, description, measurement_id[0], reg_attribute[n])
+                self.c_ao += 1
+            for i in range(5, 7):
+                for j in range(0, len(m['bankPhases'])):
+                    measurement_id = m.get("mRID")[j]
+                    name = m['tankName'] + '-' + m['bankPhases'] [j]
+                    description = "Name:" + m['tankName'][j] + ",ConductingEquipment_type:RatioTapChanger_Reg"+ ",Phase:" + m['bankPhases'][j] + ",controlAttribute:" + reg_attribute[i]
+                    self.assign_val_d("AO", 42, 3, self.c_ao, name, description, measurement_id,reg_attribute[i])
+                    self.c_ao += 1
+        
+        for m in solarpanels:
+            for k in range(0, len(m['phases'])):
+                measurement_id = m.get("mRID")
+                name = "Solar" + m['name'] + '-' + m['phases'][k] +  '-Watts-value'
+                description = "Solarpanel:" + m['name'] + ",Phase:" + m['phases'] + ",measurementID:" + measurement_id
+                self.assign_val_d("AO", 42, 3, self.c_ao, name, description, measurement_id, "PowerElectronicsConnection.p")
+                self.c_ao += 1
+                
+                name1 = "Solar" + m['name'] + '-' + m['phases'][k] +  '-VAR-value'
+                self.assign_val_d("AO", 42, 3, self.c_ao, name1, description, measurement_id, "PowerElectronicsConnection.q")
+                self.c_ao += 1
+                
+                name2 = "Solar" + m['name'] + '-' + m['phases'][k] +  '-VAR-Net-value'
+                self.assign_val_d("AO", 42, 3, self.c_ao, name2, description, measurement_id, "PowerElectronicsConnection.q")
+                self.c_ao += 1
+                
+                name3 = "Solar"+ m['name'] + '-' + m['phases'][k] +  '-Watts-Net-value'
+                self.assign_val_d("AO", 42, 3, self.c_ao, name3, description, measurement_id, "PowerElectronicsConnection.p")
+                self.c_ao += 1
+			
+        for m in batteries:
+            for l in range(0, len(m['phases'])):
+                measurement_id = m.get("mRID")
+                name = m['name'] + '-' + m['phases'][l] +  '-Watts-value'
+                description = "Battery, " + m['name'][l] + ",Phase: " + m['phases'] + ",ConductingEquipment_type:PowerElectronicConnections"
+                self.assign_val_d("AO", 42, 3, self.c_ao, name, description,measurement_id, "PowerElectronicsConnection.p")
+                self.c_ao += 1
+                name1 = m['name'] + '-' + m['phases'][l] +  '-VAR-value'
+                self.assign_val_d("AO", 42, 3, self.c_ao, name1, description,measurement_id, "PowerElectronicsConnection.q")
+                self.c_ao += 1
+            
 
         for m in switches:
             measurement_id = m.get("mRID")
             switch_attribute = attribute_map['switches']['attribute']
             for k in range(0, len(m['phases'])):
                 phase_value = list(m['phases'])
-                name = m['name']
+                name = m['name'] + "Phase:" + m['phases'][k]
                 description = "Name:" + m["name"] + ",ConductingEquipment_type:LoadBreakSwitch" + "Phase:" + phase_value[k] +",controlAttribute:"+switch_attribute
                 self.assign_val_d("DO", 12, 1, self.c_do, name, description, measurement_id, switch_attribute)
                 self.c_do += 1
 
-        # for m in fuses:
-        #     measurement_id = m.get("mRID")
-        #     switch_attribute = attribute_map['switches']['attribute']
-        #     for l in range(0, len(m['phases'])):
-        #         phase_value = list(m['phases'])
-        #         name = uuid.uuid4().hex
-        #         description = "Name:" + m["name"] + ",Phase:" + phase_value[l] + ",Attribute:" + switch_attribute + ",mRID" + measurement_id
-        #         self.assign_val_d("DO", 12, 1, self.c_do, name, description, measurement_id, switch_attribute)
-        #         self.c_do += 1
+        for m in fuses:
+            measurement_id = m.get("mRID")
+            switch_attribute = attribute_map['switches']['attribute']
+            for l in range(0, len(m['phases'])):
+                phase_value = list(m['phases'])
+                name = m['name'] + "Phase:" + m['phases'][l]
+                description = "Name:" + m["name"] + ",Phase:" + phase_value[l] + ",Attribute:" + switch_attribute + ",mRID" + measurement_id
+                self.assign_val_d("DO", 12, 1, self.c_do, name, description, measurement_id, switch_attribute)
+                self.c_do += 1
 
-        # for m in breakers:
-        #     measurement_id = m.get("mRID")
-        #     switch_attribute = attribute_map['switches']['attribute']
-        #     for n in range(0, len(m['phases'])):
-        #         phase_value = list(m['phases'])
-        #         name =uuid.uuid4().hex
-        #         description = "Name: " + m["name"] + ",Phase:" + phase_value[n] + ",ConductingEquipment_type:Breaker" + ",controlAttribute:" + switch_attribute
-        #         self.assign_val_d("DO", 12, 1, self.c_do, name, description, measurement_id, switch_attribute)
-        #         self.c_do += 1
-        #
-        # for m in reclosers:
-        #     measurement_id = m.get("mRID")
-        #     switch_attribute = attribute_map['switches']['attribute']
-        #     for k in range(0, len(m['phases'])):
-        #         phase_value = list(m['phases'])
-        #         name = uuid.uuid4().hex
-        #         description = "Recloser, " + m["name"] + "Phase: - " + phase_value[k] + ",ConductingEquipment_type:Recloser"+"controlAttribute:" + switch_attribute
-        #         self.assign_val_d("DO", 12, 1, self.c_do, name, description, measurement_id, switch_attribute)
-        #         self.c_do += 1
+        for m in breakers:
+            measurement_id = m.get("mRID")
+            switch_attribute = attribute_map['switches']['attribute']
+            for n in range(0, len(m['phases'])):
+                phase_value = list(m['phases'])
+                name = m['name'] + "Phase:" + m['phases'][n]
+                description = "Name: " + m["name"] + ",Phase:" + phase_value[n] + ",ConductingEquipment_type:Breaker" + ",controlAttribute:" + switch_attribute
+                self.assign_val_d("DO", 12, 1, self.c_do, name, description, measurement_id, switch_attribute)
+                self.c_do += 1
+        
+        for m in reclosers:
+            measurement_id = m.get("mRID")
+            switch_attribute = attribute_map['switches']['attribute']
+            for i in range(0, len(m['phases'])):
+                phase_value = list(m['phases'])
+                name = m['name'] + "Phase:" + m['phases'][i]
+                description = "Recloser, " + m["name"] + "Phase: - " + phase_value[i] + ",ConductingEquipment_type:Recloser"+"controlAttribute:" + switch_attribute
+                self.assign_val_d("DO", 12, 1, self.c_do, name, description, measurement_id, switch_attribute)
+                self.c_do += 1
 
 
         for m in energyconsumers:
             measurement_id = m.get("mRID")
             for k in range(0, len(m['phases'])):
                 phase_value = list(m['phases'])
-                name = m['name']
-                description = "EnergyConsumer, " + m["name"] + "Phase: - " + phase_value[k] 
-                self.assign_val_a("AI", 30, 1, self.c_ai, name, description, None , measurement_id)
-                self.c_ai += 1
+                name = m['name']+"phase:" + m['phases'][k]
+                description = "EnergyConsumer, " + m["name"] + "Phase: " + phase_value[k] 
+                self.assign_val_d("AO", 42, 3, self.c_ao, name, description, measurement_id, "EnergyConsumer.p")
+                self.c_ao += 1
+                
+                name1 = m['name']+"phase:" + m['phases'][k] + "control"
+                self.assign_val_d("DO", 12, 1, self.c_do, name1, description, measurement_id, "EnergyConsumer.p")
+                self.c_do += 1
 
         return self.out_json
 
