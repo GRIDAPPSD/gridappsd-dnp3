c8e3d40a2992c825b3704ca957d4debcf49dc970
Seth Gossler <seth@openenergysolutionsinc.com>
2020-07-24 13:49:45 -0700

    Accepted the wrong Spelling in the merge.

--- dnp3/service/dnp3/cim_to_dnp3.py
+++ dnp3/service/dnp3/cim_to_dnp3.py
@@ -373,7 +373,7 @@ class DNP3Mapping():
             # bank_phase = list(m['bankPhases'])
             for n in range(0, 4):
                 measurement_id = m.get("mRID")
-                name = m['bankname'] + '-' + m['bankPhases']
+                name = m['bankName'] + '-' + m['bankPhases']
                 description = "Name:" + m['bankName'] + ",ConductingEquipment_type:RatioTapChanger_Reg" +",Phase:" + m['bankPhases'] + ",Attribute:" + reg_attribute[n]
                 self.assign_val_d("AO", 42, 3, self.c_ao, name, description, measurement_id[0], reg_attribute[n])
                 self.c_ao += 1
